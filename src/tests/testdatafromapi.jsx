6566eb16663f62f41f197803043a55e0bea45fae
Raxxius <raxxman@gmail.com>
2022-12-01 09:51:57 +0000

    updated datastructure for APItest 2

--- src/tests/testdatafromapi.jsx
+++ src/tests/testdatafromapi.jsx
@@ -1,78 +1,101 @@
-
-
 /** Fake data to be sent from API */
 
-
 export const dataFromApi = {
-    event: "IFSC World Championships Moscow 2021",
-    event_underscore: "Womens Final",
-    climbers: [
-      "Stasa Gejo",
-      "Brooke Raboutou",
-      "Andrea Kümin",
-      "Elena Krasovskaia",
-      "Camilla Moroni",
-      "Natalia Grossman",
-    ],
-    typeOfClimb: "bouldering",
-    numberOfClimbs: 4,
-    src: "NAZycE9agy4",
-  }
-  
+  event: "IFSC World Championships Moscow 2021",
+  event_underscore: "Womens Boulder Final",
+  climbers: [
+    "Stasa Gejo",
+    "Brooke Raboutou",
+    "Andrea Kümin",
+    "Elena Krasovskaia",
+    "Camilla Moroni",
+    "Natalia Grossman",
+  ],
+  typeOfClimb: "bouldering",
+  numberOfClimbs: 4,
+  src: "NAZycE9agy4",
+};
+
+export const dataFromApi2 = {
+  event: "IFSC World Championships Moscow 2021",
+  event_underscore: "Lead Final",
+  climbers: [
+    "Yoshiyuki Ogata",
+    "Tomoa Narasaki",
+    "Stefano Ghisolfi",
+    "Sebastian Halenke",
+    "Martin Starnik",
+    "Luka Potocar",
+    "Jakob Schubert",
+    "Hamish Macarthur",
+    "Dinara Fakhritdinova",
+    "Brooke Raboutou",
+    "Salomé Romain",
+    "Natsuki Tanii",
+    "Jessica Pilz",
+    "Natalia Grossmann",
+    "Laura Rogora,",
+    "Chaehyun Seo",
+  ],
+  typeOfClimb: "lead",
+  numberOfClimbs: 2,
+  src: "EauI682ebv0",
+};
+
 export const dataFromServer = [
-    {
+  {
     boulder1: {
-        climber1: {
-          startsclimbing: 787,
-          endsclimbing: 1016,
-          zones: 807,
-          tops: 0
-        },
-        climber2: {
-          startsclimbing: 1065,
-          endsclimbing: 1193,
-          zones: 1102,
-          tops: 1190
-        },
-        climber3: {
-          startsclimbing: 1247,
-          endsclimbing: 1500,
-          zones: 1292,
-          tops: 1452,
-        },
-        climber4: {
-          startsclimbing: 1534,
-          endsclimbing: 1745,
-          zones: 0,
-          tops: 0,
-        },
-        climber5: {
-          startsclimbing: 1785,
-          endsclimbing: 1945,
-          zones: 1806,
-          tops: 1936,
-        },
-        climber6: {
-          startsclimbing: 2003,
-          endsclimbing: 2048,
-          zones: 2022,
-          tops: 2042,
-        },
+      climber1: {
+        startsclimbing: 787,
+        endsclimbing: 1016,
+        zones: 807,
+        tops: 0,
+      },
+      climber2: {
+        startsclimbing: 1065,
+        endsclimbing: 1193,
+        zones: 1102,
+        tops: 1190,
+      },
+      climber3: {
+        startsclimbing: 1247,
+        endsclimbing: 1500,
+        zones: 1292,
+        tops: 1452,
+      },
+      climber4: {
+        startsclimbing: 1534,
+        endsclimbing: 1745,
+        zones: 0,
+        tops: 0,
+      },
+      climber5: {
+        startsclimbing: 1785,
+        endsclimbing: 1945,
+        zones: 1806,
+        tops: 1936,
+      },
+      climber6: {
+        startsclimbing: 2003,
+        endsclimbing: 2048,
+        zones: 2022,
+        tops: 2042,
       },
     },
-    {
+  },
+  {
     boulder2: {
       climber1: {
         startsclimbing: 2120,
         endsclimbing: 2305,
         zones: 2209,
-        tops: 2299
+        tops: 2299,
       },
       climber2: {
         startsclimbing: 2386,
         endsclimbing: 2500,
         zones: 2428,
-        tops: 2494
+        tops: 2494,
       },
       climber3: {
         startsclimbing: 2560,
@@ -106,13 +129,13 @@ export const dataFromServer = [
         startsclimbing: 3336,
         endsclimbing: 3545,
         zones: 3352,
-        tops: 0
+        tops: 0,
       },
       climber2: {
         startsclimbing: 3612,
         endsclimbing: 3832,
         zones: 0,
-        tops: 0
+        tops: 0,
       },
       climber3: {
         startsclimbing: 3880,
@@ -146,38 +169,129 @@ export const dataFromServer = [
         startsclimbing: 4736,
         endsclimbing: 4808,
         zones: 4787,
-        tops: 4805
+        tops: 4805,
       },
       climber2: {
         startsclimbing: 4891,
         endsclimbing: 5125,
         zones: 4969,
-        tops: 0
+        tops: 0,
       },
       climber3: {
         startsclimbing: 5184,
         endsclimbing: 5394,
         zones: 5226,
-        tops: 0
+        tops: 0,
       },
       climber4: {
         startsclimbing: 5475,
         endsclimbing: 5671,
         zones: 5505,
-        tops: 0
+        tops: 0,
       },
       climber5: {
         startsclimbing: 5721,
         endsclimbing: 5775,
         zones: 5761,
-        tops: 5770
+        tops: 5770,
       },
       climber6: {
         startsclimbing: 5842,
         endsclimbing: 5905,
         zones: 5893,
-        tops: 5900
+        tops: 5900,
       },
     },
   },
-  ]
+];
+
+export const dataFromServer2 = [
+  {
+    men: {
+      climber1: {
+        startsclimbing: 677,
+        endsclimbing: 820,
+        score: "44+",
+      },
+      climber2: {
+        startsclimbing: 1041,
+        endsclimbing: 1266,
+        score: "46",
+      },
+      climber3: {
+        startsclimbing: 1335,
+        endsclimbing: 1584,
+        score: "34+",
+      },
+      climber4: {
+        startsclimbing: 1674,
+        endsclimbing: 2026,
+        score: "44",
+      },
+      climber5: {
+        startsclimbing: 2207,
+        endsclimbing: 2026,
+        score: "46",
+      },
+      climber6: {
+        startsclimbing: 2575,
+        endsclimbing: 2921,
+        score: "Top",
+      },
+      climber7: {
+        startsclimbing: 3035,
+        endsclimbing: 3372,
+        score: "Top",
+      },
+      climber8: {
+        startsclimbing: 3498,
+        endsclimbing: 3610,
+        score: "46",
+      },
+    },
+  },
+  {
+    women: {
+      climber1: {
+        startsclimbing: 4643,
+        endsclimbing: 4882,
+        score: "32+",
+      },
+      climber2: {
+        startsclimbing: 5315,
+        endsclimbing: 1266,
+        score: "35+",
+      },
+      climber3: {
+        startsclimbing: 5407,
+        endsclimbing: 5592,
+        score: "25+",
+      },
+      climber4: {
+        startsclimbing: 5689,
+        endsclimbing: 5963,
+        score: "32+",
+      },
+      climber5: {
+        startsclimbing: 6156,
+        endsclimbing: 6410,
+        score: "36+",
+      },
+      climber6: {
+        startsclimbing: 6506,
+        endsclimbing: 6769,
+        score: "37",
+      },
+      climber7: {
+        startsclimbing: 6866,
+        endsclimbing: 7169,
+        score: "37",
+      },
+      climber8: {
+        startsclimbing: 7354,
+        endsclimbing: 7589,
+        score: "Top",
+      },
+    }
+  }
+];
