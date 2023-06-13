5a1f6b75b0888cb43d5dc7fdaffc7f259a226b0a
Raxxius <raxxman@gmail.com>
2023-06-13 17:22:49 +0100

    update nav bar

--- README.md
+++ README.md
@@ -121,3 +121,5 @@ https://usehooks.com/useWindowSize/
 
 Icon by <a href="https://freeicons.io/profile/3">icon king1</a> on <a href="https://freeicons.io">freeicons.io</a>
 
+dropdown menu reference 
+https://www.divinectorweb.com/2021/08/pure-css-dropdown-navigation-bar.html
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,24 @@
 import Logo from "../resources/images/logo-ifsc.png";
-import Dropdown from "../resources/images/arrow-drop-down.svg";
+import DropDown from "./DropDown";
+import { useState } from "react";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false)
+
+  /* Button activity */
+  const handleShowList = () => {
+    setShowMenu(!showMenu)
+  }
+  const handleCloseList = () => {
+    setShowMenu(false)
+  }
+
   return (
     <div className="navbar">
       <img src={Logo} className="logo" alt="logo" />
       <div className="menu">
-        <div className="nav-dropdown">
-          <ul className="nav-dropbtn">
-            Competitions <img src={Dropdown} alt="dropdown tag"></img>
-            <li className="nav-dropdown-content">
-              2022
-              <ul className="nav-dropdown-side-content">
-                <li className="nav-dropdown-side-option">Moscow</li>
-                <li className="nav-dropdown-side-option">Kranj</li>
-                <li className="nav-dropdown-side-option">Briancon</li>
-                <li className="nav-dropdown-side-option">Chamonix</li>
-                <li className="nav-dropdown-side-option">Villars</li>
-                <li className="nav-dropdown-side-option">Innsbruck</li>
-                <li className="nav-dropdown-side-option">Salt Lake</li>
-                <li className="nav-dropdown-side-option">Meiringen</li>
-              </ul>
-            </li>
-            <li className="nav-dropdown-content">
-              2021
-              <ul className="nav-dropdown-side-content">
-                <li className="nav-dropdown-side-option">Moscow</li>
-                <li className="nav-dropdown-side-option">Kranj</li>
-                <li className="nav-dropdown-side-option">Briancon</li>
-                <li className="nav-dropdown-side-option">Chamonix</li>
-                <li className="nav-dropdown-side-option">Villars</li>
-                <li className="nav-dropdown-side-option">Innsbruck</li>
-                <li className="nav-dropdown-side-option">Salt Lake</li>
-                <li className="nav-dropdown-side-option">Meiringen</li>
-              </ul>
-            </li>
-            <li className="nav-dropdown-content">
-              2020
-            </li>
-          </ul>
+        <div className="nav-dropdown" onClick={handleShowList}>
+           <DropDown showMenu={showMenu}/>
         </div>
       </div>
     </div>
