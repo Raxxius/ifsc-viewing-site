ff4d0b9afd7912cda10a549935cfe08c8977bbbc
Raxxius <raxxman@gmail.com>
2023-06-25 14:37:55 +0100

    updated button functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,20 @@ import Logo from "../resources/images/logo-ifsc.png";
 import DropDown from "./DropDown";
 import { useState } from "react";
 
-function Navbar() {
-  const [showMenu, setShowMenu] = useState(false)
+function Navbar(props) {
+  const [showMenu, setShowMenu] = useState(false);
 
   /* Button activity */
   const handleShowList = () => {
-    setShowMenu(!showMenu)
-  }
-  const handleCloseList = () => {
-    setShowMenu(false)
-  }
+    setShowMenu(!showMenu);
+  };
 
   return (
     <div className="navbar">
       <img src={Logo} className="logo" alt="logo" />
       <div className="menu">
         <div className="nav-dropdown" onClick={handleShowList}>
-           <DropDown showMenu={showMenu}/>
+          <DropDown changeEvent={props.changeEvent} />
         </div>
       </div>
     </div>
