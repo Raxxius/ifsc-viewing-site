2cd1b183e8b2323992bf3d432d4e99e293da66e5
Raxxius <raxxman@gmail.com>
2023-06-25 14:37:04 +0100

    fixed a class error

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -1,18 +1,19 @@
 import Dropdown from "../resources/images/arrow-drop-down.svg";
-import { useState } from "react";
+
 
 function DropDown(props) {
+  console.log(props)
   return (
-    <nav class="nav-area">
+    <nav className="nav-area">
       <ul>
         <li>
-          <a href="#">Competitions</a>
+          <a href="#">Competitions <img src={Dropdown} /></a>
           <ul>
             <li>
               <a href="#">Lead</a>
               <ul>
                 <li>
-                  <a href="#">Moscow 2021</a>
+                  <a href="#" onClick={() => props.changeEvent(2)}>Moscow 2021</a>
                 </li>
               </ul>
             </li>
@@ -20,7 +21,7 @@ function DropDown(props) {
               <a href="#">Bouldering</a>
               <ul>
                 <li>
-                  <a href="#">Innsbruck 2021</a>
+                  <a href="#" onClick={() => props.changeEvent(1)}>Moscow 2021</a>
                 </li>
               </ul>
             </li>
@@ -37,30 +38,5 @@ function DropDown(props) {
   );
 }
 
-// function SubList(props) {
-//   return (
-//     <div className="nave-dropdown-side-content">
-//       <li className="nav-dropdown-side-option">Moscow</li>
-//     </div>
-//   );
-// }
-
-// function DropDown(props) {
-//   const className = props.showMenu ? "nav-dropdown-content" : "hidden";
-
-//   return (
-//     <ul className="nav-dropbtn">
-//       <div className="nav-dropdown-wrapper">
-//         <p>Competitions</p> <img src={Dropdown} alt="dropdown tag"></img>
-//         <li className={className}>
-//           <div className="nav-dropdown-side-wrapper">
-//             <p>Lead Climbing</p>
-//             <SubList />
-//           </div>
-//         </li>
-//       </div>
-//     </ul>
-//   );
-// }
 
 export default DropDown;
